0e6536da15c170436c5350d4fd40736bccc77f70
Dmytro Klimchuk <d664433334@gmail.com>
2025-01-10 18:20:38 +0200

    node

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+  contacts.push(...newContacts);
+  await writeContacts(contacts);
+  console.log(`${number} contacts added successfully.`);
+};
 
 generateContacts(5);
